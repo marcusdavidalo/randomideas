f756492a9b48658d9aa929b8c6bc6d005997de71
marcusdavidalo <marcusdavidalo@gmail.com>
2023-05-23 07:41:12 +0800

    update 11 files

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,157 @@
 import './css/style.css';
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('#modal');
+  const modalBtn = document.querySelector('#modal-btn');
+  const ideaList = document.querySelector('#idea-list');
+  const ideaForm = document.querySelector('#idea-form');
 
-const modal = document.querySelector('#modal');
-const modalBtn = document.querySelector('#modal-btn');
-
-function open() {
-  modal.style.display = 'block';
-}
-
-function close() {
-  modal.style.display = 'none';
-}
-
-function outsideClick(e) {
-  if (e.target === modal) {
-    close();
+  // Open modal
+  function open() {
+    modal.style.display = 'block';
   }
-}
 
-modalBtn.addEventListener('click', open);
-window.addEventListener('click', outsideClick);
+  // Close modal
+  function close() {
+    modal.style.display = 'none';
+  }
+
+  // Close modal if clicked outside
+  function outsideClick(e) {
+    if (e.target === modal) {
+      close();
+    }
+  }
+
+  // Fetch random ideas from the API
+  async function fetchIdeas() {
+    try {
+      const response = await fetch('http://localhost:8000/api/ideas');
+      const data = await response.json();
+
+      if (data.success) {
+        const ideas = data.data;
+
+        // Clear existing ideas
+        ideaList.innerHTML = '';
+
+        // Display ideas on the page
+        ideas.forEach((idea) => {
+          const card = document.createElement('div');
+          card.classList.add('card');
+
+          const deleteBtn = document.createElement('button');
+          deleteBtn.classList.add('delete');
+          deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+          deleteBtn.addEventListener('click', () => {
+            deleteIdea(idea._id);
+          });
+
+          const title = document.createElement('h3');
+          title.textContent = idea.text;
+
+          // Display tag if it exists and add the appropriate class for the tag like tag-technology, tag-business, etc. while also makin the first letter uppercase
+          const tag = document.createElement('p');
+          if (idea.tag) {
+            tag.classList.add(`tag`, `tag-${idea.tag.toLowerCase()}`); // Add the appropriate class for the tag like tag-technology, tag-business, etc.
+            tag.textContent =
+              idea.tag.charAt(0).toUpperCase() + idea.tag.slice(1); // Make the first letter uppercase
+          }
+
+          const postedBy = document.createElement('p');
+          postedBy.innerHTML = `Posted on <span class="date">${formatDate(
+            idea.date // Format date as "Month DD, YYYY"
+          )}</span> by <span class="author">${idea.username}</span>`;
+
+          card.appendChild(deleteBtn);
+          card.appendChild(title);
+          card.appendChild(tag);
+          card.appendChild(postedBy);
+
+          ideaList.appendChild(card);
+        });
+      } else {
+        console.error('Failed to fetch ideas:', data.error);
+      }
+    } catch (error) {
+      console.error('Error fetching ideas:', error);
+    }
+  }
+
+  // Delete idea
+  async function deleteIdea(ideaId) {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/ideas/${ideaId}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await response.json();
+
+      if (data.success) {
+        fetchIdeas(); // Refresh ideas after deletion
+      } else {
+        console.error('Failed to delete idea:', data.error);
+      }
+    } catch (error) {
+      console.error('Error deleting idea:', error);
+    }
+  }
+
+  // Submit idea
+  async function submitIdea(event) {
+    event.preventDefault();
+
+    const usernameInput = document.querySelector('#username');
+    const ideaTextInput = document.querySelector('#idea-text');
+    const tagInput = document.querySelector('#tag');
+
+    const username = usernameInput.value.trim();
+    const ideaText = ideaTextInput.value.trim();
+    const tag = tagInput.value.trim();
+
+    if (!username || !ideaText || !tag) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/api/ideas', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=${encodeURIComponent(
+          username
+        )}&text=${encodeURIComponent(ideaText)}&tag=${encodeURIComponent(tag)}`,
+      });
+      const data = await response.json();
+
+      if (data.success) {
+        usernameInput.value = '';
+        ideaTextInput.value = '';
+        tagInput.value = '';
+        close(); // Close the modal
+        fetchIdeas(); // Refresh ideas after submission
+      } else {
+        console.error('Failed to submit idea:', data.error);
+      }
+    } catch (error) {
+      console.error('Error submitting idea:', error);
+    }
+  }
+
+  // Format date as "Month DD, YYYY"
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  }
+
+  modalBtn.addEventListener('click', open);
+  window.addEventListener('click', outsideClick);
+  ideaForm.addEventListener('submit', submitIdea);
+
+  // Fetch ideas when the page loads
+  fetchIdeas();
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
@@ -8,14 +9,17 @@ connectDB();
 
 const app = express();
 
-//Static Folder
+// Apply CORS middleware
+app.use(cors());
+
+// Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Body Parser middleware
+// Body Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//routes
+// Routes
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to RandomIdeas API ft. MasterJake of TropangCharat64',
@@ -25,4 +29,4 @@ app.get('/', (req, res) => {
 const ideasRouter = require('./routes/ideas');
 app.use('/api/ideas', ideasRouter);
 
-app.listen(port, () => console.log(`Server listening in port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
